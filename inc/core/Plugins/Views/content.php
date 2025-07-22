e6c82680275ea0988b3efceebae8eeb749227cdb
ltmoerdani <ltmoerdani@gmail.com>
2025-07-23 01:04:49 +0700

    Refactor plugin content display logic and add index file to plugins directory

--- inc/core/Plugins/Views/content.php
+++ inc/core/Plugins/Views/content.php
@@ -1,1 +1,6 @@
-<?php _ec( $products )?>
+<?php
+if (empty($products) || $products === false || $products === null) {
+    echo '<div class="alert alert-warning">No plugins available or failed to load plugin data.</div>';
+} else {
+    _ec($products);
+}
